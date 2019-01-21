65731cb7df69d6b7606dfa12aadbfcd785080c81
Heather Hartley <hlhartley@gmail.com>
2019-01-20 23:08:06 -0700

    Adds lost logo

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,6 +4,7 @@ import './Navigation.css';
 const Navigation = (props) => {
     return (
         <div className= { props.navFixed ? "navigation-container fixed-nav" : "navigation-container" }>
+            <div className='logo'>LOST.</div>
             <a href='www.google.com'>HOME</a>
             <a href='www.google.com'>ABOUT</a>
             <a href='www.google.com'>IMAGES</a>
