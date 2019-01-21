6fabc9a175863ccf4d9f6c0a9541cbfa6bd7c3c6
Heather Hartley <hlhartley@gmail.com>
2019-01-20 18:50:49 -0700

    Adds Banner component

---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = (props) => {
+    return (
+        <div className='banner'>
+            A story about getting lost.
+        </div>
+    );
+}
+
+export default Banner;
