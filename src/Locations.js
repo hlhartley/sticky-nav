cbb862dd2c18e55c26c8adcd666153854fb7b703
Heather Hartley <hlhartley@gmail.com>
2019-01-21 14:49:31 -0700

    Adds state to fuzzy search

--- src/Locations.js
+++ src/Locations.js
@@ -16,11 +16,10 @@ class Locations extends Component {
 
     findMatches() {
         let filteredCities=this.props.cities.filter((place) => {
-            return place.city.includes(this.state.searchInput)
+            // return place.city.includes(this.state.searchInput)
+            const regex = new RegExp (this.state.searchInput, 'gi')
+            return place.city.match(regex) || place.state.match(regex)
         })
-            // const regex = new RegExp (this.state.searchInput, 'gi')
-            // return place[index].city.match(regex) || place[index].state.match(regex)
-        // })
         this.setState({filteredCities: filteredCities})
     }
     render() {
@@ -28,9 +27,9 @@ class Locations extends Component {
         return (
             <div className={ this.props.navFixed ? "main-content add-margin" : "main-content" }>
                 <h2>Locations page</h2>
-                <input type='text' className='search-cities' onChange={this.searchInput.bind(this)}></input>
+                <input type='text' placeholder='search cities / states' className='search-cities' onChange={this.searchInput.bind(this)}></input>
                 {this.state.filteredCities.map((city) => {
-                    return (<div>{city.city}</div>)
+                    return (<div>{city.city}, {city.state}</div>)
                 })}
             </div>
         );
