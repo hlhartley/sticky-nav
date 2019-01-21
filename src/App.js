bf225190746dc473fb14f2b14f849d91da9e9aa2
Heather Hartley <hlhartley@gmail.com>
2019-01-20 21:36:34 -0700

    Fix static nav bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Banner from './Banner'
+import Banner from './Banner';
+import Navigation from './Navigation';
+import MainContent from './MainContent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Banner />
+        <Navigation />
+        <MainContent />
       </div>
     );
   }
